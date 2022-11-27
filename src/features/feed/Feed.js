6ea8d605807ddd82d6ed6dfc9256a0c98e75067d
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2022-11-27 20:17:12 +0200

    Display image for each post

--- src/features/feed/Feed.js
+++ src/features/feed/Feed.js
@@ -10,19 +10,24 @@ export function Feed() {
   },[])
   
   const posts = useSelector(selectPosts);
+  console.log(posts);
   const isLoading = useSelector(feedIsLoading);
   const failedToLoad = useSelector(feedFailedToLoad);
   const errorStatus = useSelector(selectErrorStatus);
   
   if (isLoading) {
-    return <h1>Loading</h1>;
+    return <h1>I'm still loading! Wait a min!</h1>;
   }
   if (failedToLoad) {
     return <h1>Request failed... Error code {errorStatus}... sorry :(</h1>
   }
   return (
     <div>
-      {posts.map(post => <h4>{post.title}</h4>)}
+      {posts.map(post => 
+      <div>
+        <h4>{post.title}</h4>
+        <img src={post.url} style={{width: 200}}/>
+      </div>)}
     </div>
 
   )
--- src/util/Reddit.js
+++ src/util/Reddit.js
@@ -3,7 +3,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 export const generateFeed = createAsyncThunk(
   "feed/generateFeed",
   async (_, thunkAPI) => {
-    // const data = await fetch("https://api.spotify.com/v1/search?q=cher&type=artist"); - used as an test to trigger 401 error
+    // const data = await fetch("https://api.spotify.com/v1/search?q=cher&type=artist"); //- used as an test to trigger 401 error
     const data = await fetch("https://www.reddit.com/r/funny.json");
     if (!data.ok) {
       return thunkAPI.rejectWithValue(data);
