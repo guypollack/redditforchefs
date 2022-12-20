c3745a50b6cac07c5015c28653be4e446dff79b7
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2022-12-20 13:25:41 +0200

    Update dateDifference.js to use correct grammar e.g. '1 day ago' rather than '1 days ago'

--- src/util/dateDifference.js
+++ src/util/dateDifference.js
@@ -4,10 +4,10 @@ export function dateDifference(unixString) {
   if (diff < 60000) {
     return "less than a minute ago";
   } else if (diff < 3600000) {
-    return `${Math.floor(diff / 60000)} minutes ago`;
+    return Math.floor(diff / 60000) === 1 ? "1 minute ago" : `${Math.floor(diff / 60000)} minutes ago`;
   } else if (diff < 86400000) {
-    return `${Math.floor(diff / 3600000)} hours ago`;
+    return Math.floor(diff / 3600000) === 1 ? "1 hour ago" : `${Math.floor(diff / 3600000)} hours ago`;
   } else {
-    return `${Math.floor(diff / 86400000)} days ago`;
+    return Math.floor(diff / 86400000) === 1 ? "1 day ago" : `${Math.floor(diff / 86400000)} days ago`;
   }
 }
